7a8d167b4a3b747c9ef28b1f3256d4c447f1bf21
gabs <gabs712.g@gmail.com>
2025-10-03 19:21:41 -0300

    feat: add targetLocations array

---
+++ src/targetLocations.ts
@@ -0,0 +1,42 @@
+export default [
+  {
+    name: 'skeleton',
+    minX: 21.5,
+    maxX: 23.1,
+    minY: 42.7,
+    maxY: 44.1,
+    founded: false,
+  },
+  {
+    name: 'painter',
+    minX: 69.1,
+    maxX: 71,
+    minY: 76.1,
+    maxY: 77.8,
+    founded: false,
+  },
+  {
+    name: 'apple_dragon',
+    minX: 64.7,
+    maxX: 67.9,
+    minY: 41.8,
+    maxY: 43.2,
+    founded: false,
+  },
+  {
+    name: 'flame',
+    minX: 30.7,
+    maxX: 32.7,
+    minY: 65.8,
+    maxY: 67.2,
+    founded: false,
+  },
+  {
+    name: 'yellow_bird',
+    minX: 93,
+    maxX: 95,
+    minY: 97.6,
+    maxY: 99,
+    founded: false,
+  },
+]
